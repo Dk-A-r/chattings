760e1dba01b25421785a289e24d0c25a21102ba6
D Kar <dkarpov.legal@gmail.com>
2023-06-17 09:32:56 +0300

    tests

---
+++ test_app.py
@@ -0,0 +1,21 @@
+import pytest
+import streamlit as st
+
+@pytest.fixture()
+def input_invatation():
+    st.write('This chatbot is based on HuggingFace Hub.\
+    Please, enter your huggingface token.')
+    return 0
+
+@pytest.fixture()
+def input_field():
+    HUG_TOKEN = st.text_input("Token: ", key=x, type='password',
+                          placeholder='Please, enter your huggingface token')
+    return 0
+
+
+def test_input_invitation():
+    assert  input_invitation == 0
+
+def test_input_field():
+    assert test_input_fielf == 0
