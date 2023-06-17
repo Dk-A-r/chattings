aa7ff98f46ba63775a9af762d65eff97f4946c5d
D Kar <dkarpov.legal@gmail.com>
2023-06-17 12:49:26 +0300

    test fix new

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # chattings
+Настоящее приложение интегрирует free inference API одной из LLM с сайта huggingface.co (а именно, flan-t5-xxl) в приложение на Streamlit. Результатом получается достаточно простой аналог чат-бота в рамках Streamlit.
+
+Команда:
+ - Карпов Данил
+ - Катин Владимир
+ - Орлов Александр
+
+Ссылка на приложение:
+https://dk-a-r-chattings-main-4jb2u4.streamlit.app/
--- test_app.py
+++ test_app.py
@@ -1,24 +1,27 @@
 import pytest
-import streamlit as st
+import requests
 
 @pytest.fixture()
-def input_invitation():
-    st.write('This chatbot is based on HuggingFace Hub.\
-    Please, enter your huggingface token.')
-    return 0
+def check_streamlit():
+    x = requests.get('https://streamlit.io/')
+    return x.status_code
 
 @pytest.fixture()
-def input_field():
-    HUG_TOKEN = st.text_input("Token: ", type='password',
-                          placeholder='Please, enter your huggingface token')
-    return 0
+def check_model():
+    x = requests.get('https://huggingface.co/google/flan-t5-xxl')
+    return x.status_code
 
+@pytest.fixture()
+def check_app():
+    x = requests.get('https://dk-a-r-chattings-main-4jb2u4.streamlit.app/')
+    return x.status_code
 
-def test_input_invitation():
-    print(input_invitation)
-    assert  input_invitation == 0
+def test_streamlit():
+    assert check_streamlit == 200
 
+def test_model():
+    assert check_model == 200
 
-def test_input_field():
-    print(test_input_field)
-    assert test_input_field == 0
+def test_app():
+    assert check_app == 200
+
